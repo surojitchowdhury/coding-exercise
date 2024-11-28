d9f9a4859c7b2a2cfcb67aeb7c85b98e1658cb58
Luís Soares <lsoares@newstore.com>
2024-11-28 00:09:01 +0000

    use java's formated

--- java/src/main/java/com/newstore/App.java
+++ java/src/main/java/com/newstore/App.java
@@ -2,6 +2,6 @@ package com.newstore;
 
 public class App {
   public static String hello(String name) {
-    return String.format("Hello, %s!", name);
+    return "Hello, %s!".formatted(name);
   }
 }
